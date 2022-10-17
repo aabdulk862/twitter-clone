9ad92c77cf079cb8769fdb2556849253095b3222
Adam Abdulkadir <adama1862@outlook.com>
2022-10-16 23:26:13 -0400

    Sidebar and SidebarOption setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
-
+import Sidebar from './Sidebar'
 function App() {
   return (
     // BEM
     <div className="app">
       <h1>Hey What's up, Let's build a twitter clone!</h1>
+      {/* Sidebar */}
+      <Sidebar/>
+      {/* Feed */}
+      {/* Widgets */}
     </div>
   );
 }

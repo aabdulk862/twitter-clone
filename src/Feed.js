4805a2158c6e00cd9dccd33dc6d7f3760f3b6a02
Adam Abdulkadir <adama1862@outlook.com>
2022-10-17 11:04:37 -0400

    Widget Tweet Embed

--- src/Feed.js
+++ src/Feed.js
@@ -6,20 +6,14 @@ import Post from "./Post";
 function Feed() {
   return (
     <div className="feed">
-      {/*Header  */}
       <div className="feed__header">
         <h2>Home</h2>
       </div>
-      {/* TweetBox */}
-      <TweetBox/>
-      <Post/>
-      {/* Post */}
-      {/* Post */}
-      {/* Post */}
-      {/* Post */}
-      {/* Post */}
-      {/* Post */}
-      {/* Post */}
+      <TweetBox />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
     </div>
   );
 }
--- src/Widgets.js
+++ src/Widgets.js
@@ -1,11 +1,21 @@
-import React from 'react'
-import './Widgets.css'
+import React from "react";
+import "./Widgets.css";
+import SearchIcon from "@mui/icons-material/Search";
+import {TwitterTweetEmbed, TwitterTimelineEmbed, TwitterShareButton} from 'react-twitter-embed';
 function Widgets() {
   return (
-    <div className='widgets'>
-      <h2>Widgets</h2>
+    <div className="widgets">
+      <div className="widgets__input">
+        <SearchIcon className="widgets__searchIcon" />
+        <input placeholder="Search Twitter" type="text" />
+      </div>
+
+      <div className="widgets__widgetsContainer">
+        <h2>What's happening</h2>
+        <TwitterTweetEmbed tweetId={"1582022751887314944"} />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Widgets
+export default Widgets;

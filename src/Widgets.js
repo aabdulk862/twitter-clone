3460e06f804a10e3aa30ad784ef3f2bcb59c6cad
Adam Abdulkadir <adama1862@outlook.com>
2022-10-17 11:27:22 -0400

    Widget live twitter feed

--- src/Widgets.js
+++ src/Widgets.js
@@ -1,7 +1,11 @@
 import React from "react";
 import "./Widgets.css";
 import SearchIcon from "@mui/icons-material/Search";
-import {TwitterTweetEmbed, TwitterTimelineEmbed, TwitterShareButton} from 'react-twitter-embed';
+import {
+  TwitterTweetEmbed,
+  TwitterTimelineEmbed,
+  TwitterShareButton,
+} from "react-twitter-embed";
 function Widgets() {
   return (
     <div className="widgets">
@@ -13,6 +17,18 @@ function Widgets() {
       <div className="widgets__widgetsContainer">
         <h2>What's happening</h2>
         <TwitterTweetEmbed tweetId={"1582022751887314944"} />
+        <TwitterTimelineEmbed
+          sourceType="profile"
+          screenName="adam_862"
+          options={{ height: 400 }}
+        />
+        <TwitterShareButton
+          url={"https://github.com/aabdulk862"}
+          options={{
+            text: "Check out this twitter clone made with #React",
+            via: "adam_862",
+          }}
+        />
       </div>
     </div>
   );

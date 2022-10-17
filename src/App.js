3e3224f30f33ebebde246e3c99332257ad03542c
Adam Abdulkadir <adama1862@outlook.com>
2022-10-17 00:00:41 -0400

    Starting on other components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import './App.css';
 import Sidebar from './Sidebar'
+import Feed from './Feed'
 function App() {
   return (
     // BEM
     <div className="app">
-      <h1>Hey What's up, Let's build a twitter clone!</h1>
-      {/* Sidebar */}
       <Sidebar/>
-      {/* Feed */}
+      <Feed/>
       {/* Widgets */}
     </div>
   );
